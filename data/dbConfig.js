859f6971cd00b5f9252e795d412b3e768eb3093a
Carlos Baez <c.baez1223@yahoo.com>
2020-10-16 22:36:09 -0400

    updated dbConfig file

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,6 @@
 const knex = require("knex")
 
 const knexfile = require("../knexfile")
-const environment = process.env.NODE_ENV || "development"
+const environment = process.env.DB_CONNECT || "development"
 
 module.exports = knex(knexfile[environment])
